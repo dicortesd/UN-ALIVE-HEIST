7a6177f6cb2cb10d3853e0432343423de14ffa0b
Santiago Cassiano Rozo <scassiano@unal.edu.co>
2023-07-27 15:30:58 -0500

    Primera version score

---
+++ UnAlive Heist/Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    private TextMeshProUGUI textMesh;
+    private int score;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.text = score.ToString();
+        timer = timer + Time.deltaTime;
+
+        //Cada 0.5 segundos incrementar el score
+        if (timer >= 0.5f) {
+            score++;
+            timer = 0.0f;
+        }
+    }
+}
